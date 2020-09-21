601ef55edb31da6cb071a06ccd0b27ce70f8cfe6
Andrea C <andrea.campisi@t3lab.it>
2020-09-21 19:20:30 +0200

    up

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import numpy as np
-from flask_ngrok import run_with_ngrok
+#from flask_ngrok import run_with_ngrok
 from flask import Flask,render_template,request
 app = Flask(__name__)
 #run_with_ngrok(app)  # Start ngrok when app is run
