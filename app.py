6b56ce648003efa2d8bdd492769ccdda9f2348ce
Andrea C <andrea.campisi@t3lab.it>
2020-09-21 20:05:46 +0200

    update with figure

--- app.py
+++ app.py
@@ -4,15 +4,26 @@ from flask import Flask,render_template,request
 app = Flask(__name__)
 #run_with_ngrok(app)  # Start ngrok when app is run
 
+
+def load_data():
+    df = pd.read_csv("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv")
+    df["data"] = [el[0:10] for el in df["data"].values.tolist()]
+    return df 
+
 # https://iot-app-course.herokuapp.com/
 @app.route('/')
 def index():
+    df = load_data()
+    select = ["deceduti","totale_casi","dimessi_guariti","variazione_totale_positivi"]
+    fig = plot_plotly(dfplot,x ="data", y=["deceduti","dimessi_guariti","totale_positivi"],title="Andamento Nazionale")    
+    
     return render_template('index.html', 
                             name="Manuel",
                             surname="Rucci",
                             eta="26",
                             sesso="Maschio",
 							collaboratore = "Sono Ugo e collaboro all'app")
+                            fig= convert_plotly_fig_to_json(fig))
 
 if __name__ == "__main__":
     app.run()
