96c2d25eb6344c76cca011ae66b2f2cf9a6f1d78
Andrea C <andrea.campisi@t3lab.it>
2020-09-21 19:18:09 +0200

    fix guinicorn package missing

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask,render_template,request
 app = Flask(__name__)
 #run_with_ngrok(app)  # Start ngrok when app is run
 
+# https://iot-app-course.herokuapp.com/
 @app.route('/')
 def index():
     name = "Mi Chiamo Manuel Rucci FINE Lezione di Lunedi"
