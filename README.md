e0a70eb681ab1de5a8ae571ca69fd3ce3ae273bc
manuelrucci <rucci@localhost.com>
2020-09-23 10:13:26 +0200

    up

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # iot-app
 IOT flask app
 HO ciao
+
+
+## Setup Heroku
+
+```
+Procfile: web: gunicorn app:app --log-file=-
+Procfile: web: gunicorn --worker-class socketio.sgunicorn.GeventSocketIOWorker --log-file=- websocketApp:app
+```
---
+++ websocketApp.py
@@ -0,0 +1,62 @@
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import threading
+import pandas as pd
+import time
+from utils import *
+import json
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app)
+
+def load_data():
+    df = pd.read_csv("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv")
+    df["data"] = [el[0:10] for el in df["data"].values.tolist()]
+    return df 
+def update_func():
+    dfplot = []
+    ii = 3
+    df = load_data()
+    start=True
+    while(start):
+        if df.values.shape[0]<(ii-3):
+            start=False
+        else:
+            ii =ii+1
+        time.sleep(0.1)
+        dfplot = df.iloc[0:ii]
+        fig1 = plot_plotly(dfplot,x ="data", y=["deceduti","totale_casi","dimessi_guariti"],title="Andamento Nazionale")    
+        fig2 = plot_plotly(dfplot,x ="data", y=["variazione_totale_positivi"],title="Variazione totale positivi")   
+        fig1_json = json.loads(convert_plotly_fig_to_json(fig1))
+        fig2_json = json.loads(convert_plotly_fig_to_json(fig2))
+        socketio.emit("stream", {   "date": df.iloc[ii]["data"] ,
+                                    "fig1_data": fig1_json["data"] , 
+                                    "fig1_layout": fig1_json["layout"],
+                                    "fig2_data": fig2_json["data"] , 
+                                    "fig2_layout": fig2_json["layout"],
+                                })
+def timer():
+    i=0
+    while(True):
+        time.sleep(0.5)
+        socketio.emit("stream", "Clock: " + str(i))
+        i = i + 1
+
+
+x = threading.Thread(target=update_func)
+x.start()
+time.sleep(2)
+
+@app.route('/')
+def sessions():
+    return render_template('websocket.html')
+
+@socketio.on('connected')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
