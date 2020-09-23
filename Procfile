5133b8c0dbbc90b4ffab41d01075b78f2e7809b7
manuelrucci <rucci@localhost.com>
2020-09-23 11:16:29 +0200

    up

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --worker-class socketio.sgunicorn.GeventSocketIOWorker --log-file=- websocketApp:app
+web: gunicorn -k eventlet --log-file=- websocketApp:app
--- websocketApp.py
+++ websocketApp.py
@@ -59,4 +59,4 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app) #, debug=True)
